5c5358a3aca2d4e47dfc48213dda0a83c5cf135d
RionaFeb <85821021+RionaFeb@users.noreply.github.com>
2021-06-26 13:19:27 +0800

    Update Balloonwithlight.cpp

--- Balloonwithlight.cpp
+++ Balloonwithlight.cpp
@@ -6,11 +6,21 @@ const int font2=(int)GLUT_BITMAP_TIMES_ROMAN_10;
 void init()
 {
     glClearColor(0.0f,0.0f,0.1f,0.0f);
-    GLfloat g_light_position[] = {20.0, 10.0, 10.0, 0.0};
+    GLfloat mat_specular[]={1.0f, 1.0f, 1.0f, 1.0f} ;
+    GLfloat mat_diffuse[]={1.0f, 1.0f, 1.0f, 1.0f };
+    GLfloat mat_ambient[]={ 0.2f, 0.2f, 0.2f, 1.0f };
+    GLfloat g_light_position[] = { -1.5f, 1.0f, -4.0f, 1.0f };
+    GLfloat mat_shininess[] = { 70.0 };
+    glShadeModel (GL_SMOOTH);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_LIGHTING);
+    //glEnable(GL_COLOR_MATERIAL);
     glEnable(GL_LIGHT0);
-    //glLightfv(GL_LIGHT0, GL_POSITION, g_light_position);
+    glLightfv(GL_LIGHT0, GL_POSITION, g_light_position);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+    glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
 
 
 
@@ -25,16 +35,13 @@ void renderBitmapString(float x,float y,void *font,const char *string)
         glutBitmapCharacter(font,*c);
     }
 }
-void DrawBalloon(float radiusX, float radiusY,int posX,int posY)
+void line(float x, float y)
 {
     glColor3f(1.0,1.0,1.0);
-	glBegin(GL_POLYGON);
-	for(int i=0;i<360;i++)
-	{
-	  float theta=i*3.142/180;
-	  glVertex2f(posX+0.1*cos(theta)*radiusX,posY+0.1*sin(theta)*radiusY);          //half230, 230, 250
-	}
-	glEnd();
+    glBegin(GL_LINES);
+    glVertex3f(x,y,0.0);
+    glVertex3f(x,y-3.0,0.0);
+    glEnd();
 }
 void balloon(float red, float green, float blue)
 {
@@ -46,33 +53,29 @@ void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
     glColor3f(1.0,1.0,0.0);
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
     double w = glutGet( GLUT_WINDOW_WIDTH );
     double h = glutGet( GLUT_WINDOW_HEIGHT );
-    gluPerspective(60, w / h, 1.0, 100.0);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    gluLookAt(2, 0, 6, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
     //balloon
     glPushMatrix();
-    glTranslatef(-4.0,1.0,0.0);
+    glTranslatef(-2.0,1.0,0.0);
     balloon(1.0,1.0,0.0);
     glPopMatrix();
 
     glPushMatrix();
-    glTranslatef(-3.0,2.0,0.0);
+    glTranslatef(-1.0,2.0,0.0);
     balloon(1.0,1.0,1.0);
     glPopMatrix();
 
     glPushMatrix();
-    glTranslatef(-2.5,0.8,0.0);
-    balloon(0.5,0.5,0.5);
+    glTranslatef(-1.0,0.4,0.0);
+    balloon(0.4,0.4,0.4);
     glPopMatrix();
 
+    glPushMatrix();
+    glTranslatef(0.1,0.8,0.0);
+    balloon(0.8,0.4 ,0.1);
+    glPopMatrix();
     //Message
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -92,14 +95,23 @@ void display(void)
 }
 void reshape(int w, int h)
 {
-   glViewport(0, 0,w,h);
+    glViewport(0, 0,(GLsizei)w,(GLsizei)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    if(w<=h)
+        gluPerspective(60, h / w, 1.0, 100.0);
+    else
+        gluPerspective(60, w / h, 1.0, 100.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(2, 0, 6, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 }
 
 
 int main(int argc,char**argv)
 {
     glutInit(&argc,argv);
-    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB|GLUT_DEPTH);
     glutInitWindowPosition(200,200);
     glutInitWindowSize(700,400);
     glutCreateWindow("New Year Card");
@@ -111,3 +123,4 @@ int main(int argc,char**argv)
 
 return 0;
 }
+
